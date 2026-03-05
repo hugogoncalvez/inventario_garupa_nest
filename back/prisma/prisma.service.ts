0637cf227fb2937b464479df8cf353a0136f3ca9
hugogoncalvez <hugogoncalvez@gmail.com>
2026-03-05 08:28:45 -0300

    Fix: implemented correct mariadb connection pool for Prisma v7 adapter

--- back/prisma/prisma.service.ts
+++ back/prisma/prisma.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import * as mariadb from 'mariadb';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import 'dotenv/config';
 
 @Injectable()
@@ -8,7 +10,18 @@ export class PrismaService
     extends PrismaClient
     implements OnModuleInit {
     constructor() {
-        super();
+        const pool = mariadb.createPool({
+            host: process.env.DATABASE_HOST,
+            port: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : 3306,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+            connectionLimit: 10,
+        });
+
+        const adapter = new PrismaMariaDb(pool);
+
+        super({ adapter });
     }
 
     async onModuleInit() {
