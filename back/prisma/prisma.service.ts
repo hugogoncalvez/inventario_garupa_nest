6cd29c51c55b3fefe7d70089b5651e092b18b460
hugogoncalvez <hugogoncalvez@gmail.com>
2026-03-05 08:23:58 -0300

    Fix: standard database connection using DATABASE_URL in schema and service

--- back/prisma/prisma.service.ts
+++ back/prisma/prisma.service.ts
@@ -8,15 +8,7 @@ export class PrismaService
     extends PrismaClient
     implements OnModuleInit {
     constructor() {
-        super({
-            adapter: new PrismaMariaDb({
-                host: process.env.DATABASE_HOST,
-                user: process.env.DATABASE_USER,
-                password: process.env.DATABASE_PASSWORD,
-                database: process.env.DATABASE_NAME,
-                connectionLimit: 10,
-            }),
-        });
+        super();
     }
 
     async onModuleInit() {
