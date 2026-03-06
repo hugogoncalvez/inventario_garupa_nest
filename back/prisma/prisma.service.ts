2b2b11011fb618bc7bba759e16cf6b2d3266d074
hugogoncalvez <hugogoncalvez@gmail.com>
2026-03-06 08:26:33 -0300

    Migrate DB from Clever Cloud to Aiven: SSL with ca.pem + keepAlive

--- back/prisma/prisma.service.ts
+++ back/prisma/prisma.service.ts
@@ -2,12 +2,17 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import 'dotenv/config';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class PrismaService
     extends PrismaClient
     implements OnModuleInit {
     constructor() {
+        // Cargar certificado SSL de Aiven (ca.pem está en back/ca.pem)
+        const sslCa = fs.readFileSync(path.join(__dirname, '../../ca.pem'));
+
         const adapter = new PrismaMariaDb({
             host: process.env.DATABASE_HOST,
             port: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : 3306,
@@ -15,16 +20,18 @@ export class PrismaService
             password: process.env.DATABASE_PASSWORD,
             database: process.env.DATABASE_NAME,
             connectionLimit: 3,
-            allowPublicKeyRetrieval: true,
             connectTimeout: 20000,
             acquireTimeout: 20000,
-            idleTimeout: 60000,
+            keepAliveDelay: 30000,
+            ssl: {
+                ca: sslCa,
+            },
         });
         super({ adapter });
     }
 
     async onModuleInit() {
-        console.log('CONECTANDO A BASE DE DATOS CLEVER CLOUD...');
+        console.log('CONECTANDO A BASE DE DATOS AIVEN...');
         try {
             await this.$connect();
             console.log('¡CONEXIÓN EXITOSA CON LA BASE DE DATOS!');
